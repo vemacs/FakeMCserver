1b5b21d36573416f626481045a01db56fa9373c4
vemacs <gentoo@outlook.com>
2014-05-29 11:52:26 -0600

    parse colors and newline

--- src/main/java/me/vemacs/fakemcserver/Main.java
+++ src/main/java/me/vemacs/fakemcserver/Main.java
@@ -9,6 +9,8 @@ public class Main {
     static Properties prop = new Properties();
     public static StatusResponse response = null;
 
+    public static final char ESCAPE = '\u00A7';
+
     public static void main(String[] args) throws Exception {
         File config = new File("server.properties");
 
@@ -30,7 +32,7 @@ public class Main {
                     Integer.parseInt(prop.getProperty("protocol")),
                     Integer.parseInt(prop.getProperty("max")),
                     Integer.parseInt(prop.getProperty("online")),
-                    prop.getProperty("description")
+                    replaceColors(prop.getProperty("description")).replace("\\n", "\n")
             );
         }
 
@@ -41,5 +43,20 @@ public class Main {
             port = 25565;
         }
         new SLPServer(port).run();
+    }
+
+    public static String replaceColors(String text) {
+        char[] chrarray = text.toCharArray();
+        for (int index = 0; index < chrarray.length; index++) {
+            char chr = chrarray[index];
+            if (chr != '&')
+                continue;
+            if ((index + 1) == chrarray.length)
+                break;
+            char forward = chrarray[index + 1];
+            if ((forward >= '0' && forward <= '9') || (forward >= 'a' && forward <= 'f') || (forward >= 'k' && forward <= 'r'))
+                chrarray[index] = ESCAPE;
+        }
+        return new String(chrarray);
     }
 }
